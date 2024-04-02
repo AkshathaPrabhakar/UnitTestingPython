2aeccb52b66e4d5cc9db02fc5e423f2ff8f86573
AkshathaPrabhakar <152319463+AkshathaPrabhakar@users.noreply.github.com>
2024-04-02 10:20:15 +0200

    Add files via upload

---
+++ RunAllTest.py
@@ -0,0 +1,23 @@
+import unittest
+from test_1 import CourseraPageTest
+from test_2 import HTMLContentCheckerTest
+from test_3 import TestLogin
+
+def suite():
+    loader = unittest.TestLoader()
+    test_suite = unittest.TestSuite()
+    # Add the specific test cases
+    test_suite.addTests(loader.loadTestsFromTestCase(CourseraPageTest))
+    test_suite.addTests(loader.loadTestsFromTestCase(HTMLContentCheckerTest))
+    test_suite.addTests(loader.loadTestsFromTestCase(TestLogin))
+    return test_suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    # Run the entire suite
+    result = runner.run(suite())
+
+
+
+
+
